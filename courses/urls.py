c3aae99a1e36b2279dcda04ea084ef0fdead5f04
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-08-13 09:21:36 +0545

    fix:: url issues

--- courses/urls.py
+++ courses/urls.py
@@ -7,10 +7,11 @@ router.register(r'categories', CategoryViewSet)
 router.register(r'courses', CourseViewSet)
 router.register(r'chapters', ChapterViewSet)
 router.register(r'lessons', LessonViewSet)
-router.register(r'watch', LessonDetailView )
+# router.register(r'watch', LessonDetailView )
 
 urlpatterns = [
     path('', include(router.urls)),
     path('lesson/<int:lesson_id>/partial-key/', UserLessonKeyUpdateView.as_view(), name='lesson-partial-key'),
     path('lesson/playback/<int:lesson_id>/', UserLessonKeyUpdateView.as_view(), name='lesson-playback'),
+    path('lessons/<int:pk>/watch/', LessonDetailView.as_view(), name='lesson-detail'),
 ]
