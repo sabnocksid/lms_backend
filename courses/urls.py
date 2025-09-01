ce5ad941698cd5a0ab3ad78fa8ad6a92690474c7
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-09-01 17:51:34 +0545

    fix:: issue

--- courses/urls.py
+++ courses/urls.py
@@ -3,17 +3,17 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     CategoryViewSet,
     LessonViewSet,
-    LessonSectionViewSet,
     LessonResourceViewSet,
     LessonProgressViewSet,
+    LessonReviewViewSet
 )
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'lessons', LessonViewSet)
-router.register(r'sections', LessonSectionViewSet)
 router.register(r'resources', LessonResourceViewSet)
 router.register(r'progress', LessonProgressViewSet)
+router.register(r'reviews', LessonReviewViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
