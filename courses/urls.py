d552647f2e408d9e366ba8800b088edfa87dd673
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-09-11 15:12:36 +0545

    fix:: issue in url

--- courses/urls.py
+++ courses/urls.py
@@ -5,7 +5,7 @@ from .views import CategoryViewSet, CourseViewSet, CourseRatingViewSet
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'courses', CourseViewSet)
-router.register(r'ratings', CourseRatingViewSet)
+router.register(r'ratings', CourseRatingViewSet, basename='ratings')
 
 urlpatterns = [
     path('', include(router.urls)),
