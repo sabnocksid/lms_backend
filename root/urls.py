91cb8ccd61701b2f05f32b5e14442192a8494bcd
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-09-13 14:25:28 +0545

    fix:: naming issue

--- root/urls.py
+++ root/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("api/courses/", include(("courses.urls"))),
 
     # lesson API
-    path("api/", include("lessons.urls")),
+    path("api/lessons", include("lesson.urls")),
 
     # Schema & Documentation
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
