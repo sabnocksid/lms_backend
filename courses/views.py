7d9e72b22fecff486921b76c16670707dd71e63d
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-09-13 07:45:44 +0545

    fix:: course views

--- courses/views.py
+++ courses/views.py
@@ -21,6 +21,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
 
 
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all().order_by("-date_added")
     permission_classes = [IsInstructorOrAdminOrReadOnly]
@@ -37,6 +38,63 @@ class CourseViewSet(viewsets.ModelViewSet):
         elif self.action in ["create", "update", "partial_update"]:
             return CourseCreateUpdateSerializer
         return CoursePreviewSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                {
+                    "success": False,
+                    "message": "Course creation failed",
+                    "errors": serializer.errors,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {
+                "success": True,
+                "message": "Course created successfully",
+                "data": serializer.data,
+            },
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if not serializer.is_valid():
+            return Response(
+                {
+                    "success": False,
+                    "message": "Course update failed",
+                    "errors": serializer.errors,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        self.perform_update(serializer)
+        return Response(
+            {
+                "success": True,
+                "message": "Course updated successfully",
+                "data": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {
+                "success": True,
+                "message": "Course deleted successfully",
+            },
+            status=status.HTTP_204_NO_CONTENT,
+        )
 
 
 
