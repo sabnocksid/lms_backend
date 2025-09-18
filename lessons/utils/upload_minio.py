0ea5761907ebd2d70c06dec85a0fbdd89017cef4
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-09-18 17:36:35 +0545

    fix:: views logic

---
+++ lessons/utils/upload_minio.py
@@ -0,0 +1,19 @@
+import boto3
+from django.conf import settings
+from botocore.exceptions import NoCredentialsError, ClientError
+
+def upload_file_to_minio(file_obj, file_name, bucket=None):
+    bucket = bucket or settings.AWS_STORAGE_BUCKET_NAME
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    )
+
+    try:
+        s3_client.upload_fileobj(file_obj, bucket, file_name)
+        return f"{settings.AWS_S3_ENDPOINT_URL}/{bucket}/{file_name}"
+    except (NoCredentialsError, ClientError) as e:
+        print("MinIO Upload Error:", e)
+        return None
--- lessons/views.py
+++ lessons/views.py
@@ -1,14 +1,50 @@
+# views.py
 from rest_framework import generics
 from .models import Lesson
 from .serializers import LessonSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
+from .utils.minio_upload import upload_file_to_minio
 
 class LessonListCreateView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all().order_by('-created_at')
     serializer_class = LessonSerializer
-    parser_classes = [MultiPartParser, FormParser]  
+    parser_classes = [MultiPartParser, FormParser]
+
+    def perform_create(self, serializer):
+        data = self.request.data.copy()
+
+        if "video" in self.request.FILES:
+            video_file = self.request.FILES["video"]
+            video_url = upload_file_to_minio(video_file, f"videos/{video_file.name}")
+            if video_url:
+                data["video"] = video_url
+
+        if "material" in self.request.FILES:
+            material_file = self.request.FILES["material"]
+            material_url = upload_file_to_minio(material_file, f"materials/{material_file.name}")
+            if material_url:
+                data["material"] = material_url
+
+        serializer.save(**data)
 
 class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     parser_classes = [MultiPartParser, FormParser]
+
+    def perform_update(self, serializer):
+        data = self.request.data.copy()
+
+        if "video" in self.request.FILES:
+            video_file = self.request.FILES["video"]
+            video_url = upload_file_to_minio(video_file, f"videos/{video_file.name}")
+            if video_url:
+                data["video"] = video_url
+
+        if "material" in self.request.FILES:
+            material_file = self.request.FILES["material"]
+            material_url = upload_file_to_minio(material_file, f"materials/{material_file.name}")
+            if material_url:
+                data["material"] = material_url
+
+        serializer.save(**data)
