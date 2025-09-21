a1f517f5bd0b311d5c0a0d402ba8ee51d6f5e742
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-09-21 07:28:03 +0545

    fix:: issue

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LessonViewSet, ChapterViewSet
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r"lessons", LessonViewSet, basename="lessons")
 router.register(r"chapters", ChapterViewSet, basename="chapters")
 
 urlpatterns = [
-    path("api/", include(router.urls)),
+    path("", include(router.urls)),
 ]
