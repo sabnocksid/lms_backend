9773b89672f5ac6c87862b00be7b6a7f724f49cc
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-09-21 17:18:08 +0545

    fix:: issue

--- root/settings.py
+++ root/settings.py
@@ -81,7 +81,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8001",
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://localhost:5500"
+    "http://localhost:5500",
     "http://127.0.0.1:5501"
 ]
 CORS_ALLOW_CREDENTIALS = True
