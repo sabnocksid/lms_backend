09ca9c5fcfabcad4b270704179a46d78f57439d6
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-14 07:30:51 +0545

    add:: email authentication added

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.backends import ModelBackend
-from .models import CustomUser
+from django.contrib.auth.backends import BaseBackend
+from accounts.models import CustomUser
 
-class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        email = kwargs.get('email') or username
-        if email is None or password is None:
-            return None
+class EmailBackend(BaseBackend):
+    def authenticate(self, request, email=None, password=None, **kwargs):
         try:
             user = CustomUser.objects.get(email=email)
+            if user.check_password(password):
+                return user
         except CustomUser.DoesNotExist:
             return None
-        if user.check_password(password) and self.user_can_authenticate(user):
-            return user
-        return None
+
+    def get_user(self, user_id):
+        try:
+            return CustomUser.objects.get(pk=user_id)
+        except CustomUser.DoesNotExist:
+            return None
