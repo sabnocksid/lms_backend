8e129efc60d1627efd53e763e063b3af9a811b8b
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-14 07:54:41 +0545

    fix:: naming conflict in celery.py

--- root/celery.py
+++ root/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
-app = Celery('lms_backend')
+app = Celery('root')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
