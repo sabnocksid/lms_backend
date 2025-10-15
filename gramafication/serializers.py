b5050c53aa01bbe51d803c882fb21e83adbd3f77
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-15 18:16:59 +0545

    fix:: learners profile serializers

--- gramafication/serializers.py
+++ gramafication/serializers.py
@@ -4,6 +4,7 @@ from .models import (
     Task, TaskCompletion
 )
 
+from lessons.utils.upload_minio import upload_file_to_minio
 
 class BadgeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -70,10 +71,24 @@ class LeaderboardSerializer(serializers.ModelSerializer):
 
 
 class LearnerProfileUpdateSerializer(serializers.ModelSerializer):
+    profile_image_file = serializers.FileField(write_only=True, required=False)
+
     class Meta:
         model = LearnerProfile
-        fields = ['date_of_birth', 'profile_image']  
+        fields = ["date_of_birth", "profile_image", "profile_image_file"]
         extra_kwargs = {
-            'profile_image': {'required': False},
-            'date_of_birth': {'required': False},
+            "profile_image": {"read_only": True},  # only set internally after upload
         }
+
+    def update(self, instance, validated_data):
+        file_obj = validated_data.pop("profile_image_file", None)
+
+        if file_obj:
+            key = upload_file_to_minio(file_obj, f"learners/profile_images/{file_obj.name}")
+            instance.profile_image = key  # store public URL in CharField
+
+        if "date_of_birth" in validated_data:
+            instance.date_of_birth = validated_data["date_of_birth"]
+
+        instance.save()
+        return instance
