83d094d7f0f3a59399ae261d00f0b218883af176
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-15 10:29:42 +0545

    feat:: auto create learners profile for student users

---
+++ accounts/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import CustomUser
+from gramafication.models import LearnerProfile
+
+@receiver(post_save, sender=CustomUser)
+def create_learner_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'student':
+        LearnerProfile.objects.create(
+            user=instance,
+            full_name=instance.full_name
+        )
+
+@receiver(post_save, sender=CustomUser)
+def save_learner_profile(sender, instance, **kwargs):
+    if instance.role == 'student' and hasattr(instance, 'learner_profile'):
+        instance.learner_profile.save()
