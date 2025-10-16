f5ec276a649921efa44c449936808f85c15d8664
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-16 12:14:19 +0545

    fix:: issue

---
+++ quizes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import QuizListView, QuizDetailView, QuizAttemptCreateView
+
+urlpatterns = [
+    path('quizzes/', QuizListView.as_view(), name='quiz-list'),
+    path('quizzes/<int:pk>/', QuizDetailView.as_view(), name='quiz-detail'),
+    path('attempt/', QuizAttemptCreateView.as_view(), name='quiz-attempt'),
+]
