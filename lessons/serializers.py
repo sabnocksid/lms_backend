1fa76430fc0c33a44f9c19b0c5aa871248d8ee04
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-24 11:42:59 +0545

    add:: completion percentage

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -307,24 +307,29 @@ class LessonDetailSerializer(serializers.ModelSerializer):
         return None
 
     def get_course_completion_percentage(self, obj):
-        user = self.context.get("user") 
+        user = self.context.get("user")  
         if not user:
-            return 0  
+            return 0 
 
-        total_lessons = obj.course.lessons.count() 
+        total_lessons = obj.course.lessons.count()  
         completed_lessons = 0 
 
         for lesson in obj.course.lessons.all():
-            total_chapters = lesson.chapters.count()  
-            completed_chapters = lesson.chapters.filter(progress__user=user, progress__completed=True).count()  
+            total_chapters = lesson.chapters.count()
+            completed_chapters = lesson.chapters.filter(progress__user=user, progress__completed=True).count()
 
+            print(f"Lesson: {lesson.title}, Total Chapters: {total_chapters}, Completed Chapters: {completed_chapters}")  
+            
             if total_chapters > 0 and completed_chapters == total_chapters:
-                completed_lessons += 1  
+                completed_lessons += 1
 
         if total_lessons == 0:
             return 0  
 
-        return (completed_lessons / total_lessons) * 100
+        completion_percentage = (completed_lessons / total_lessons) * 100
+        print(f"Course Completion Percentage: {completion_percentage}")  
+
+        return completion_percentage
 
     def create(self, validated_data):
         file_obj = validated_data.pop("thumbnail_file", None)
