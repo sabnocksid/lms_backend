6b318635ced631d99a084bc145a70b3f5e221b3e
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-24 12:10:47 +0545

    add:: completion percentage

--- lessons/views.py
+++ lessons/views.py
@@ -2,13 +2,13 @@ from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Lesson, Chapter, ChapterProgress
-from .serializers import LessonDetailSerializer, ChapterSerializer, ChapterProgressSerializer
+from .serializers import LessonDetailSerializer, LessonWithProgressSerializer, ChapterSerializer, ChapterProgressSerializer
 from django.utils import timezone
 
 
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all().order_by("-created_at")
-    serializer_class = LessonDetailSerializer
+    serializer_class = LessonWithProgressSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_serializer_context(self):
