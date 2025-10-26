f889056b10fa936706e9feba5742c9e2e9f4423e
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-26 11:35:54 +0545

    fix:: quiz result serializers and viewset

--- quizes/urls.py
+++ quizes/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuizViewSet, QuizCreateAPIView, QuizAttemptSubmitAPIView
+from .views import QuizViewSet, QuizCreateAPIView, QuizAttemptView
 
 router = DefaultRouter()
 router.register(r'quizzes', QuizViewSet, basename='quiz')
 
 urlpatterns = [
     path('quizzes/create/', QuizCreateAPIView.as_view(), name='quiz-create'),
-    path('attempts/', QuizAttemptSubmitAPIView.as_view(), name='quiz-attempt'),
+    path('attempts/', QuizAttemptView.as_view(), name='quiz-attempt'),
     path('', include(router.urls)),
 ]
