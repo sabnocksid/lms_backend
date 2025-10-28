98775f6cc0dc06e0de69e0732b134798e265b437
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-28 07:16:52 +0545

    add:: performance trend for last 30 days

--- gramafication/views.py
+++ gramafication/views.py
@@ -373,15 +373,11 @@ class DashboardView(APIView):
                 )
 
                 performance_score = round(
-                    (0.4 * course_engagement) +
-                    (0.3 * quiz_activity) +
-                    (0.3 * accuracy_today), 2
+                    min(1.0, (0.4 * course_engagement) + (0.3 * quiz_activity) + (0.3 * accuracy_today)), 2
                 )
 
-                performance_trend.append({
-                    "date": str(day),
-                    "score": performance_score
-                })
+                raw_score = (0.4 * course_engagement) + (0.3 * quiz_activity) + (0.3 * accuracy_today)
+                performance_score = round(max(0, min(1.0, raw_score)), 2)
 
             performance_trend.reverse()
             stats_box_data["performance_last_30_days"] = performance_trend
