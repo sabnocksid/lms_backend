eeecc0352831177dd219d7279bfb25ba7df3304c
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-10-30 08:16:26 +0545

    add:: new response for admin/instructor

--- gramafication/views.py
+++ gramafication/views.py
@@ -257,6 +257,7 @@ class DashboardView(APIView):
                     "quiz_activity": round(quiz_activity, 2),
                     "accuracy_today": round(accuracy_today, 2),
                     "score": round(avg_perf_score, 2),
+                    "student_count": student_count
                 })
 
             # Stats box for admin/instructor: aggregated over all students
