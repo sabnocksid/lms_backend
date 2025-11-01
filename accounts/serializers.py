6cf8561f0d34c410f81c7d1d3111c47b1d4b9eac
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-01 08:06:17 +0545

    add:: api with role filtering

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,11 +11,11 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['email', 'full_name', 'password', 'role']
+        fields = ['email', 'full_name', 'password']
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = CustomUser.objects.create_user(password=password, **validated_data)
+        user = CustomUser.objects.create_user(password=password, role='student', **validated_data)
 
         signer = TimestampSigner()
         token = signer.sign(user.pk)
