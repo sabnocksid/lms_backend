28df9ed500c8911fe8d43a899a223aaeb588c2ea
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-04 13:47:25 +0545

    add:: url integrated

--- root/asgi.py
+++ root/asgi.py
@@ -5,7 +5,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
 import django
 django.setup() 
 
-import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from discussion.routing import websocket_urlpatterns
--- root/middleware.py
+++ root/middleware.py
@@ -7,32 +7,22 @@ User = get_user_model()
 
 
 class JWTAuthMiddleware:
+
     def __init__(self, inner):
         self.inner = inner
 
-    async def __call__(self, scope, receive, send):
-        scope["user"] = None
-
-        subprotocols = scope.get("subprotocols", [])
-        if len(subprotocols) == 2 and subprotocols[0] == "Bearer":
-            token = subprotocols[1]
-            try:
-                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-                scope["user"] = await database_sync_to_async(User.objects.get)(id=payload["user_id"])
-            except Exception:
-                scope["user"] = None
-
-        inner = self.inner(scope)
-        return await inner(receive, send)
+    def __call__(self, scope):
+        return JWTAuthMiddlewareInstance(scope, self.inner)
 
 
 class JWTAuthMiddlewareInstance:
     def __init__(self, scope, inner):
         self.scope = dict(scope)
-        self.inner = inner
+        self.inner = inner  
 
     async def __call__(self, receive, send):
         self.scope["user"] = None
+
         subprotocols = self.scope.get("subprotocols", [])
         if len(subprotocols) == 2 and subprotocols[0] == "Bearer":
             token = subprotocols[1]
@@ -41,5 +31,6 @@ class JWTAuthMiddlewareInstance:
                 self.scope["user"] = await database_sync_to_async(User.objects.get)(id=payload["user_id"])
             except Exception:
                 self.scope["user"] = None
+
         inner = self.inner(self.scope)
         return await inner(receive, send)
