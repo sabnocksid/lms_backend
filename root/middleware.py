6f79b80ae611d893b79abf03b27a21846a3d089c
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-04 16:05:54 +0545

    fix:: middleware and asgi error

--- root/middleware.py
+++ root/middleware.py
@@ -1,6 +1,7 @@
 import jwt
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from channels.db import database_sync_to_async
 
 User = get_user_model()
@@ -10,39 +11,36 @@ class JWTAuthMiddleware:
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        scope["user"] = None
+        scope["user"] = AnonymousUser()
 
         query_string = scope.get("query_string", b"").decode()
         token = None
+        
         if "token=" in query_string:
             token = query_string.split("token=")[1].split("&")[0]
+        
+        if not token:
+            subprotocols = scope.get("subprotocols", [])
+            if len(subprotocols) >= 2 and subprotocols[0] == "Bearer":
+                token = subprotocols[1]
 
         if token:
-            try:
-                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-                scope["user"] = await database_sync_to_async(User.objects.get)(id=payload["user_id"])
-            except Exception:
-                scope["user"] = None
+            user = await self.get_user_from_token(token)
+            if user:
+                scope["user"] = user
 
         return await self.inner(scope, receive, send)
 
+    @database_sync_to_async
+    def get_user_from_token(self, token):
 
-class JWTAuthMiddlewareInstance:
-    def __init__(self, scope, inner):
-        self.scope = dict(scope)
-        self.inner = inner
-
-    async def __call__(self, receive, send):
-        self.scope["user"] = None
-
-        subprotocols = self.scope.get("subprotocols", [])
-        if len(subprotocols) == 2 and subprotocols[0] == "Bearer":
-            token = subprotocols[1]
-            try:
-                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-                self.scope["user"] = await database_sync_to_async(User.objects.get)(id=payload["user_id"])
-            except Exception:
-                self.scope["user"] = None
-
-        inner = self.inner(self.scope)
-        return await inner(receive, send)
+        try:
+            payload = jwt.decode(
+                token, 
+                settings.SECRET_KEY, 
+                algorithms=["HS256"]
+            )
+            user = User.objects.get(id=payload["user_id"])
+            return user
+        except (jwt.ExpiredSignatureError, jwt.DecodeError, User.DoesNotExist):
+            return None
