6e5b6b6eed92cd5ddb92fcd442b28ad1124b4ba2
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-04 13:06:29 +0545

    set the settings module before any Django import

--- root/asgi.py
+++ root/asgi.py
@@ -1,11 +1,15 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")  
+
+import django
+django.setup() 
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 from discussion.routing import websocket_urlpatterns
 from discussion.middleware import JWTAuthMiddleware
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
