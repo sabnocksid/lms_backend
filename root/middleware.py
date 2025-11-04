ed5684afbd9611d6672593e098e9ddb7376de8ca
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-04 13:38:58 +0545

    add:: url integrated

--- root/middleware.py
+++ root/middleware.py
@@ -1,42 +1,32 @@
-# discussion/middleware.py
 import jwt
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from channels.db import database_sync_to_async
-from accounts.models import CustomUser
 
-@database_sync_to_async
-def get_user_from_payload(payload):
-    return CustomUser.objects.get(id=payload["user_id"])
+User = get_user_model()
 
 class JWTAuthMiddleware:
-
-
     def __init__(self, inner):
         self.inner = inner
 
     def __call__(self, scope):
         return JWTAuthMiddlewareInstance(scope, self.inner)
 
+
 class JWTAuthMiddlewareInstance:
     def __init__(self, scope, inner):
-        self.scope = scope
+        self.scope = dict(scope)
         self.inner = inner
 
     async def __call__(self, receive, send):
-        query_string = self.scope.get("query_string", b"").decode()
-        token = None
-        for param in query_string.split("&"):
-            if param.startswith("token="):
-                token = param.split("=")[1]
-                break
-
         self.scope["user"] = None
-        if token:
+        subprotocols = self.scope.get("subprotocols", [])
+        if len(subprotocols) == 2 and subprotocols[0] == "Bearer":
+            token = subprotocols[1]
             try:
                 payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-                self.scope["user"] = await get_user_from_payload(payload)
+                self.scope["user"] = await database_sync_to_async(User.objects.get)(id=payload["user_id"])
             except Exception:
                 self.scope["user"] = None
-
         inner = self.inner(self.scope)
         return await inner(receive, send)
