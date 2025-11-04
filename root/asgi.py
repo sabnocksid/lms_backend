aa61b87b8d7d6247a6aa8e5d546a389ff16f92a5
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-04 13:55:16 +0545

    add:: url integrated

--- root/asgi.py
+++ root/asgi.py
@@ -1,4 +1,11 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")  
+
+import django
+django.setup() 
+
+import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from discussion.routing import websocket_urlpatterns
@@ -9,8 +16,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,  # normal HTTP
+    "http": django_asgi_app,  
     "websocket": JWTAuthMiddleware(
         URLRouter(websocket_urlpatterns)
     ),
 })
+
