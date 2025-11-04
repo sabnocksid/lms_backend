3cd1b7fd81f19422021bfc3fb0515ffbf8246d3e
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-04 12:26:39 +0545

    add:: search functionality in leaderboard

--- root/asgi.py
+++ root/asgi.py
@@ -1,10 +1,12 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import discussion.routing
+import discussion.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
+django.setup()  
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
