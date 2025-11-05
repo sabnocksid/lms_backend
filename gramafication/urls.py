bf0232def6d8cf2cb445fa7b951c3145a44a572e
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-05 20:47:50 +0545

    add:: seach functionality

--- gramafication/urls.py
+++ gramafication/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("learners/me/", LearnerProfileDetailView.as_view(), name="learner-profile-detail"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path("leaderboard/", FullLeaderboardView.as_view(), name="leaderboard"),
-    path("enroll/<int:course_id>", EnrollCourseView.as_view(), name="enroll-course"),
+    path("enroll/<int:course_id>/", EnrollCourseView.as_view(), name="enroll-course"),
     path("my-enrollments/", MyEnrollmentsView.as_view(), name="user-enrollments"),
 
     path("course/<int:course_id>/progress/", CourseGamificationView.as_view(), name="course-gamification-progress"),
