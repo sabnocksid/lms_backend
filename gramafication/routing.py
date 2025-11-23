84a3e62fe8edbae707b50b57acb1906501de29b9
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-23 17:21:59 +0545

    debug:: notification socket

--- gramafication/routing.py
+++ gramafication/routing.py
@@ -2,5 +2,13 @@ from django.urls import re_path
 from .consumers import NotificationConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/notifications/$', NotificationConsumer.as_asgi()),
+    re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
 ]
+
+
+# from django.urls import re_path
+# from .consumers import DiscussionConsumer
+
+# websocket_urlpatterns = [
+#     re_path(r'ws/discussion/(?P<room_name>\d+)/$', DiscussionConsumer.as_asgi()),
+# ]
