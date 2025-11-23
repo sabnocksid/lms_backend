08f5fe92de4b92e8788732bb445df221e9b0422b
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-23 10:13:03 +0545

    fix:: asgi.py import error

--- discussion/routing.py
+++ discussion/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import re_path
 from .consumers import DiscussionConsumer
 
 websocket_urlpatterns = [
-    path("ws/discussion/<room_name>/", DiscussionConsumer.as_asgi()),
+    re_path(r'ws/discussion/(?P<thread_id>\d+)/$', DiscussionConsumer.as_asgi()),
 ]
--- root/asgi.py
+++ root/asgi.py
@@ -1,52 +1,21 @@
 import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")  
-
-import django
-django.setup() 
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from discussion.routing import websocket_urlpatterns
-from gramafication.routing import websocket_urlpatterns
+from discussion.routing import websocket_urlpatterns as discussion_ws
+from gramafication.routing import websocket_urlpatterns as gramafication_ws
 from .middleware import JWTAuthMiddleware
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,  
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
-            URLRouter(websocket_urlpatterns)
+            URLRouter(
+                discussion_ws + gramafication_ws 
+            )
         )
     ),
 })
-
-
-
-# import os
-# import django
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.security.websocket import AllowedHostsOriginValidator
-# from channels.auth import AuthMiddlewareStack   # <--- THIS
-
-# from discussion.routing import websocket_urlpatterns as discussion_ws
-# from gramafication.routing import websocket_urlpatterns as gamification_ws
-# from .middleware import JWTAuthMiddleware
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
-# django.setup()
-
-# django_asgi_app = get_asgi_application()
-
-# application = ProtocolTypeRouter({
-#     "http": django_asgi_app,
-#     "websocket": AllowedHostsOriginValidator(
-#         JWTAuthMiddleware(
-#             URLRouter(gamification_ws + discussion_ws)
-#         )
-#     ),
-# })
-
