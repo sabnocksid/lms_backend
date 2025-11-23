baa8879b9284709b945f8c44a90291c1c7620536
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-23 10:16:24 +0545

    fix:: asgi.py import error

--- discussion/routing.py
+++ discussion/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import DiscussionConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/discussion/(?P<thread_id>\d+)/$', DiscussionConsumer.as_asgi()),
+    re_path(r'ws/discussion/(?P<room_name>\d+)/$', DiscussionConsumer.as_asgi()),
 ]
--- root/asgi.py
+++ root/asgi.py
@@ -3,18 +3,17 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from discussion.routing import websocket_urlpatterns as discussion_ws
-from gramafication.routing import websocket_urlpatterns as gramafication_ws
+from gramafication.routing import websocket_urlpatterns as gamification_ws
 from .middleware import JWTAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
             URLRouter(
-                discussion_ws + gramafication_ws 
+                discussion_ws + gamification_ws
             )
         )
     ),
