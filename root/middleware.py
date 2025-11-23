0ffce823b9823e7be8aa3da4f43b20ee98c64c13
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-23 17:44:29 +0545

    debug:: notification socket

--- root/middleware.py
+++ root/middleware.py
@@ -1,10 +1,13 @@
 from urllib.parse import parse_qs
 from channels.middleware import BaseMiddleware
 from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+import logging
+
+logger = logging.getLogger(__name__)
 
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        # Lazy imports to avoid AppRegistryNotReady
         from django.contrib.auth import get_user_model
         from rest_framework_simplejwt.tokens import UntypedToken
         from jwt import decode as jwt_decode
@@ -13,7 +16,7 @@ class JWTAuthMiddleware(BaseMiddleware):
         User = get_user_model()
         query_string = parse_qs(scope["query_string"].decode())
         token = query_string.get("token")
-        scope["user"] = None
+        scope["user"] = AnonymousUser()
 
         if token:
             try:
@@ -21,7 +24,8 @@ class JWTAuthMiddleware(BaseMiddleware):
                 decoded_data = jwt_decode(token[0], settings.SECRET_KEY, algorithms=["HS256"])
                 user = await database_sync_to_async(User.objects.get)(id=decoded_data["user_id"])
                 scope["user"] = user
-            except Exception:
-                scope["user"] = None
+            except Exception as e:
+                logger.warning(f"JWTAuthMiddleware failed: {e}")
+                scope["user"] = AnonymousUser()
 
         return await super().__call__(scope, receive, send)
