47aaf6a5bae918728aa6b0509416c2d612296c46
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-23 18:05:31 +0545

    debug:: notification socket

--- root/asgi.py
+++ root/asgi.py
@@ -1,23 +1,20 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-
-from root.middleware import JWTAuthMiddleware
-from discussion.routing import websocket_urlpatterns as discussion_ws
-from notifications.routing import websocket_urlpatterns as notifications_ws
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from root.middleware import JWTAuthMiddleware
+from discussion.routing import websocket_urlpatterns as discussion_ws
+from notifications.routing import websocket_urlpatterns as notifications_ws
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
-            URLRouter(
-                discussion_ws + notifications_ws
-            )
+            URLRouter(discussion_ws + notifications_ws)
         )
     ),
 })
--- root/middleware.py
+++ root/middleware.py
@@ -1,30 +1,41 @@
-from urllib.parse import parse_qs
+import jwt
 from channels.middleware import BaseMiddleware
-from channels.db import database_sync_to_async
-from django.contrib.auth.models import AnonymousUser
 from django.conf import settings
-from rest_framework_simplejwt.tokens import UntypedToken
-from jwt import decode as jwt_decode
+from urllib.parse import parse_qs
 
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
+        from django.contrib.auth.models import AnonymousUser  # <-- moved inside
 
-        query_string = parse_qs(scope["query_string"].decode())
-        token_list = query_string.get("token")
+        query_string = scope.get("query_string", b"").decode()
+        query_params = parse_qs(query_string)
+        token = query_params.get("token", [None])[0]
 
-        scope["user"] = AnonymousUser()
-
-        if token_list:
-            token = token_list[0]
-
+        if token:
             try:
-                UntypedToken(token)
-                decoded = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-                user = await database_sync_to_async(User.objects.get)(id=decoded["user_id"])
+                payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+
+                from django.contrib.auth import get_user_model  # <-- safe here
+                User = get_user_model()
+
+                user = await self.get_user(payload["user_id"], User)
                 scope["user"] = user
             except Exception:
                 scope["user"] = AnonymousUser()
+        else:
+            scope["user"] = AnonymousUser()
 
         return await super().__call__(scope, receive, send)
+
+    @staticmethod
+    async def get_user(user_id, User):
+        from channels.db import database_sync_to_async
+
+        @database_sync_to_async
+        def inner():
+            try:
+                return User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                return None
+
+        return await inner()
