7227e7dd85b87477393075e51b50bcce90f70dc1
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-23 10:37:47 +0545

    fix:: asgi.py import error

--- root/asgi.py
+++ root/asgi.py
@@ -2,23 +2,19 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
-
-from root.middleware import JWTAuthMiddleware  # your JWT middleware
-from discussion.routing import websocket_urlpatterns as discussion_ws
-from notifications.routing import websocket_urlpatterns as notification_ws
+from root.middleware import JWTAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
 
 django_asgi_app = get_asgi_application()
 
+from discussion.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
-            URLRouter(
-                discussion_ws + notification_ws
-            )
+            URLRouter(websocket_urlpatterns)
         )
     ),
 })
