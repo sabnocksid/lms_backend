cff1310041c353dbb9cbb1b2a36d4e7777f3eb56
Sidharth Poudel <sidharthpoudel04@gmail.com>
2025-11-23 09:08:16 +0545

    fix:: asgi.py import error

--- root/asgi.py
+++ root/asgi.py
@@ -1,36 +1,52 @@
+# import os
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")  
+
+# import django
+# django.setup() 
+
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from discussion.routing import websocket_urlpatterns
+# from gramafication.routing import websocket_urlpatterns
+# from .middleware import JWTAuthMiddleware
+
+# django_asgi_app = get_asgi_application()
+
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,  
+#     "websocket": AllowedHostsOriginValidator(
+#         JWTAuthMiddleware(
+#             URLRouter(websocket_urlpatterns)
+#         )
+#     ),
+# })
+
+
+
 import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")  
-
 import django
-django.setup() 
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from discussion.routing import websocket_urlpatterns
-from gramafication.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack   
+
+from discussion.routing import websocket_urlpatterns as discussion_ws
+from gramafication.routing import websocket_urlpatterns as gamification_ws
 from .middleware import JWTAuthMiddleware
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,  
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
-            URLRouter(websocket_urlpatterns)
+            URLRouter(gamification_ws + discussion_ws)
         )
     ),
 })
 
-
-
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            gramafication.routing.websocket_urlpatterns
-        )
-    ),
-})
